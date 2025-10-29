38510764f0827bada98dc6797ae8d2faedfddadc
xianmihu <miguelangelminambres@sekou.es>
2025-10-29 19:04:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
+  transpilePackages: ['lucide-react'],
 }
 
 module.exports = nextConfig
